0d2cbbd1387fdabf831d506861cf4a91a25dc68e
evertoncastro <evertoncastro.sp@gmail.com>
2018-05-31 17:53:41 -0300

    Add inquirer lib to ask input

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # playingDataStructure
 
-Run python main.py on each module to see each datastructure implementation
+### Setup
+
+Check if you have virtualenv dependency installed in your environment
+
+`virtualenv --version`
+
+If you do not have it, make the installation running the command below:
+
+`pip install virtualenv`
+
+With virtualenv installed create a virtual env inside your project folder: 
+
+```
+cd projectFolder/
+virtualenv env 
+source env/bin/activate
+pip install -r requirements.txt
+```
--- binary_tree/main.py
+++ binary_tree/main.py
@@ -1,16 +1,26 @@
+import inquirer
+import random
+import re
+from inquirer.themes import GreenPassion
 from tree import Tree
 
 if __name__ == '__main__':
+    pattern_validation = re.compile("^([0-9]+)+$")
+
+    questions = [
+        inquirer.Text('size', message="What's the node quantity for the tree?")
+    ]
+    answers = inquirer.prompt(questions, theme=GreenPassion())
+    size = int(answers['size'])
+
+    # Random with non-repeated
+    random_values = random.sample(xrange(1, 99), size)
+
     tree = Tree()
-    tree.add_value(8)
-    tree.add_value(16)
-    tree.add_value(5)
-    tree.add_value(7)
-    tree.add_value(7)
-    tree.add_value(4)
-    tree.add_value(1)
-    tree.add_value(2)
+    for val in random_values:
+        tree.add_value(val)
+
     tree.traverse_in_order()
     tree.traverse_pre_order()
     tree.traverse_post_order()
-    tree.search(16)
+
